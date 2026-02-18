815664ac3724654f05c6dbd12e8dd4f60e9be14a
lauroware <lauroware@gmail.com>
2026-02-17 23:50:48 -0300

    lauro

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const corsOptions = {
   origin: true,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
